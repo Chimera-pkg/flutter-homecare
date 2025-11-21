9e007f1956bea51ccb0e6d810022502f18559dfe
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-11-21 11:14:23 +0700

    refactor(home_health_screening): remove  authorization token from screening services request

--- lib/features/home_health_screening/data/datasources/home_health_screening_remote_datasource.dart
+++ lib/features/home_health_screening/data/datasources/home_health_screening_remote_datasource.dart
@@ -1,8 +1,6 @@
-import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:m2health/const.dart';
 import 'package:m2health/features/home_health_screening/data/models/screening_category_model.dart';
-import 'package:m2health/utils.dart';
 
 class HomeHealthScreeningRemoteDatasource {
   final Dio dio;
@@ -10,10 +8,8 @@ class HomeHealthScreeningRemoteDatasource {
   HomeHealthScreeningRemoteDatasource(this.dio);
 
   Future<List<ScreeningCategoryModel>> getScreeningServices() async {
-    final token = await Utils.getSpString(Const.TOKEN);
     final response = await dio.get(
-      '${Const.URL_API}/screening-services', // Maps to routes/v1/screening-services.ts
-      options: Options(headers: {'Authorization': 'Bearer $token'}),
+      '${Const.URL_API}/screening-services',
     );
 
     return (response.data['data'] as List)
